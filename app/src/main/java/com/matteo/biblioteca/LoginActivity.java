d0279e7f27707592e45c9e4d25f4ec0753b92b9e
Matteo Mammoliti <matti.mm04@gmail.com>
2023-04-16 20:33:56 +0200

    Fifth commit

--- app/src/main/java/com/matteo/biblioteca/LoginActivity.java
+++ app/src/main/java/com/matteo/biblioteca/LoginActivity.java
@@ -7,52 +7,33 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText username, password;
+    EditText email, password;
     Button login, register;
-    DBConnection database;
+    CheckBox loginState;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username = (EditText) findViewById(R.id.username_l);
+        email = (EditText) findViewById(R.id.email_l);
         password = (EditText) findViewById(R.id.password_l);
         login = (Button) findViewById(R.id.login_l);
         register = (Button) findViewById(R.id.register_l);
-        database = new DBConnection(this);
+        loginState = (CheckBox) findViewById(R.id.loginstate);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                String user_name = username.getText().toString();
-                String pass = password.getText().toString();
-
-                if(user_name.equals("") || pass.equals(""))
-                {
-                    Toast.makeText(LoginActivity.this, "Compila tutti i campi!", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    boolean checkuserpass = database.checkusernamepassword(user_name, pass);
-                    if(checkuserpass == true)
-                    {
-                        Toast.makeText(LoginActivity.this, "Accesso completato. Caricamento.....", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), HomePageActivity.class);
-                        intent.putExtra("name", username.getText().toString());
-                        startActivity(intent);
-                    }
-                    else
-                    {
-                        Toast.makeText(LoginActivity.this, "Credenziali non valide!", Toast.LENGTH_SHORT).show();
-                    }
-                }
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
             }
         });
 
--- app/src/main/java/com/matteo/biblioteca/MainActivity.java
+++ app/src/main/java/com/matteo/biblioteca/MainActivity.java
@@ -1,5 +1,6 @@
 package com.matteo.biblioteca;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -8,7 +9,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,9 +44,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                String user_name = username.getText().toString();
-                String pass = password.getText().toString();
-
             }
         });
 
@@ -50,13 +59,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void registerNewAccount(String nome, String cognome, String email, String password) {
-        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
-        progressDialog.setCancelable(false);
-        progressDialog.setIndeterminate(false);
-        progressDialog.setTitle("Registrazione in corso");
-        progressDialog.show();
+        ProgressBar progressBar= new ProgressBar(MainActivity.this);
+        //TODO progressBar.setCancelable(false);
+        progressBar.setIndeterminate(false);
+        //TODO progressBar.setTitle("Registrazione in corso");
+        progressBar.setVisibility(View.VISIBLE);
 
-        String URL = "localhost";
+        String URL = "localhost/dashboard/bibliotecaApp/register.php";
+        StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equals("Successfully Registered"))
+                {
+                    //TODO progressBar.dismiss();
+                    Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+                else
+                {
+                    //TODO progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //TODO progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> param = new HashMap<>();
+                param.put("nome", nome);
+                param.put("cognome", cognome);
+                param.put("email", email);
+                param.put("password", password);
+                return param;
+            }
+        };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(3000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        MySingleton.getmInstance(MainActivity.this).addToRequestQueue(request);
 
     }
 }
